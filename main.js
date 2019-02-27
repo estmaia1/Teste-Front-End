47ecde354343cde468ec9b8248da54025adf53f1
estmaia1 <estmaia@gmail.com>
2019-02-26 22:42:29 -0300

    Added shadow effect and button when hovering products

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-var h1 = document.getElementsByTagName("h1");
-
 // Populando a prateleira de produtos, através da grid do bootstrap
 $(document).ready(function() {
   $.ajax({
@@ -18,14 +16,23 @@ $(document).ready(function() {
         $("#products").append(
           // "<li>" + data.productName + ":" + data.productId + "</li>"
           // Criação dos cards em colunas reponsivas
-          '<div class="col-sm-6 col-md-6 col-lg-4 col-xl-3 d-flex justify-content-center">\n<div class="card mt-4" style="width: 18rem;">\n  <img src=" ' +
+          '<div class="col-sm-6 col-md-6 col-lg-4 col-xl-3 d-flex justify-content-center">\n<div class="card mb-5" style="width: 18rem;">\n  <img src=" ' +
             data.imageUrl +
-            ' " class="card-img-top" alt="...">\n  <div class="card-body d-flex flex-column justify-content-end">\n    <h6 class="card-title">' +
+            ' " class="card-img-top" alt="...">\n  <div class="card-body d-flex flex-column">\n    <h6 class="card-title">' +
             data.productName +
             '</h6>\n <p class="card-text">R$ <span id="value">' +
             data.price.toFixed(2).replace(".", ",") +
             '</span></p> \n<a href="#" class="btn btn-light btn-lg py-3" id="btnComprar">COMPRAR<i class="fas fa-shopping-cart ml-2"></i></a>\n </div>\n </div>\n</div>'
         );
+      });
+
+      // Efeito de sombra ao posicionar o cursor sobre o produto
+      $(".card").hover(function() {
+        $(this).toggleClass("shadow bg-white mb-5 mb-2 rounded border-0");
+        //Exibe/oculta o botão de compra
+        $(this)
+          .find("a")
+          .toggle();
       });
     },
     error: function(error) {
