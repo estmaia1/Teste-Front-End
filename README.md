4233bb7b912d00136560d3a6b938ec4f32911281
estmaia1 <estmaia@gmail.com>
2019-02-28 21:54:26 -0300

    Added dynamic history of purchases based on current user and updated README.md

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
-Teste Front-End
-===============
+# Teste Front-End
+
 Este projeto conta com uma simples página de e-commerce, utilizando recursos HTML, CSS, JavaScript, JQuery e Bootstrap.
 
 Para utilizar a página corretamente, basta abrir o arquivo "index.html" no seu navegador.
 
-Funcionalidades
----------------
+## Funcionalidades
+
 - Consumo de lista de produtos de api via ajax, populando uma grid de cards bootstrap.
 - Transporte de id do produto selecionado pelo hash, de modo a popular a página individual de cada produto com uma call ajax.
+- Resgate de última compra através de GET utilizando informações de usuário fornecidas.
+- Interface de carrinho com listagem de produtos em grid (preenchimento dinâmico ainda não implementado).
 - Formulário para inscrição em newsletter, realizado através de POST.
 
-Instalação
-----------
+## Instalação
+
 - Baixar ou manter os arquivos na estrutura fornecida e iniciar a execução pelo arquivo "index.html".
--- javascript/master.js
+++ javascript/master.js
@@ -1,5 +1,6 @@
 // Métodos comuns a todas as páginas
 
+// Envio de dados para newsletter
 $("#btnNewsletter").click(function() {
   $.ajax({
     url: "http://api.vtexcrm.com.br/corebiz/dataentities/TE/documents",
@@ -17,6 +18,56 @@ $("#btnNewsletter").click(function() {
     },
     error: function(result) {
       alert("Verifique as informações inseridas!");
+    }
+  });
+});
+
+// Preenchimento das informações do usuário no momento do carregamento da página
+// Ao implementar uma função de login, este método deverá ser alterado
+$(document).ready(function() {
+  function fillList() {
+    $.ajax({
+      type: "GET",
+      datatype: "JSON",
+      contentType: "application/json",
+      url: "https://desolate-brushlands-20405.herokuapp.com/api/product/1",
+      data: JSON.stringify({
+        productId: "value",
+        productName: "value",
+        imageUrl: "value",
+        price: "value"
+      }),
+      success: function(response) {
+        $("#list").append(
+          '<div class="col-3">\n<img src="' +
+            response.imageUrl +
+            '" class="w-100 h-100">\n</div>\n<div class="col-6">\n<h5 class="mb-0">' +
+            response.productName +
+            '</h5>\n</div>\n<div id="lastVl" class="col-3">\nR$ <span>' +
+            response.price.toFixed(2).replace(".", ",") +
+            "</span>\n</div>"
+        );
+      },
+      error: function(error) {
+        alert("error");
+      }
+    });
+  }
+
+  $.ajax({
+    url: "https://desolate-brushlands-20405.herokuapp.com/api/user",
+    type: "POST",
+    contentType: "application/json; charset=utf-8",
+    data: JSON.stringify({
+      email: "cdd@corebiz.com.br"
+    }),
+    success: function(result) {
+      $("#loginName").text(result.name);
+      $("#loginMail").text(result.email);
+      fillList(result.shopping.pop().id);
+    },
+    error: function(result) {
+      alert("error");
     }
   });
 });
